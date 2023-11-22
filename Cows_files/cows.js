95ee9fa7b4b26cb6de6945383593d7508515b105
Natalie <natalizuk09@gmail.com>
2023-11-22 17:15:04 +0300

    Pop-ups and designed changes

--- Cows_files/cows.js
+++ Cows_files/cows.js
@@ -1,22 +1,24 @@
-var kBoardWidth = 9; //игра 9 на 9
-var kBoardHeight = 9;
-var kPieceWidth = 50; //длина одной ячейки
-var kPieceHeight= 50;
-var kPixelWidth = 1 + (kBoardWidth * kPieceWidth); //прорисовать сбоку поля
-var kPixelHeight= 1 + (kBoardHeight * kPieceHeight);
+let kBoardWidth = 9;
+let kBoardHeight = 9;
+let kPieceWidth = 50;
+let kPieceHeight= 50;
+let kPixelWidth = 1 + (kBoardWidth * kPieceWidth);
+let kPixelHeight = 1 + (kBoardHeight * kPieceHeight);
 
-var gCanvasElement;
-var gDrawingContext;
-var gPattern;
-var cow;
+let gCanvasElement;
+let gDrawingContext;
+let cowImg;
+let farmImg;
 
-var gPieces;
-var gNumPieces;
-var gSelectedPieceIndex;
-var gSelectedPieceHasMoved;
-var gMoveCount;
-var gMoveCountElem;
-var gGameInProgress;
+let popupElement;
+
+let gPieces;
+let gNumPieces;
+let gSelectedPieceIndex;
+let gSelectedPieceHasMoved;
+let gMoveCount;
+let gMoveCountElem;
+let gGameInProgress;
 
 function Cell(row, column) {
     this.row = row;
@@ -24,9 +26,9 @@ function Cell(row, column) {
 }
 
 function getCursorPosition(e) {
-    /* returns Cell with .row and .column properties */
-    var x;
-    var y;
+    let x;
+    let y;
+
     if (e.pageX != undefined && e.pageY != undefined) {
 	    x = e.pageX;
 	    y = e.pageY;
@@ -35,20 +37,21 @@ function getCursorPosition(e) {
 	    x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
 	    y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
     }
+
     x -= gCanvasElement.offsetLeft;
     y -= gCanvasElement.offsetTop;
     x = Math.min(x, kBoardWidth * kPieceWidth);
     y = Math.min(y, kBoardHeight * kPieceHeight);
-    var cell = new Cell(Math.floor(y/kPieceHeight), Math.floor(x/kPieceWidth));
+    let cell = new Cell(Math.floor(y / kPieceHeight), Math.floor(x / kPieceWidth));
+
     return cell;
 }
 
 function halmaOnClick(e) {
-    var cell = getCursorPosition(e);
+    let cell = getCursorPosition(e);
 
-    for (var i = 0; i < gNumPieces; i++) {
-	    if ((gPieces[i].row == cell.row) && 
-	        (gPieces[i].column == cell.column)) {
+    for (let i = 0; i < gNumPieces; i++) {
+	    if ((gPieces[i].row == cell.row) && (gPieces[i].column == cell.column)) {
 	        clickOnPiece(i);
 	        return;
 	    }
@@ -59,12 +62,10 @@ function halmaOnClick(e) {
 
 function clickOnEmptyCell(cell) {
     if (gSelectedPieceIndex == -1) { return; }
-    var rowDiff = Math.abs(cell.row - gPieces[gSelectedPieceIndex].row);
-    var columnDiff = Math.abs(cell.column - gPieces[gSelectedPieceIndex].column);
+    let rowDiff = Math.abs(cell.row - gPieces[gSelectedPieceIndex].row);
+    let columnDiff = Math.abs(cell.column - gPieces[gSelectedPieceIndex].column);
 
     if ((rowDiff <= 1) && (columnDiff <= 1)) {
-	/* we already know that this click was on an empty square,
-	   so that must mean this was a valid single-square move */
 	    gPieces[gSelectedPieceIndex].row = cell.row;
 	    gPieces[gSelectedPieceIndex].column = cell.column;
 	    gMoveCount += 1;
@@ -74,10 +75,8 @@ function clickOnEmptyCell(cell) {
 	    return;
     }
 
-    if ((((rowDiff == 2) && (columnDiff == 0)) ||
-	 ((rowDiff == 0) && (columnDiff == 2)) ||
-	 ((rowDiff == 2) && (columnDiff == 2))) && 
-	    isThereAPieceBetween(gPieces[gSelectedPieceIndex], cell)) {
+    if ((((rowDiff == 2) && (columnDiff == 0)) || ((rowDiff == 0) && (columnDiff == 2)) ||
+	 ((rowDiff == 2) && (columnDiff == 2))) && isThereAPieceBetween(gPieces[gSelectedPieceIndex], cell)) {
 	    /* this was a valid jump */
 	    if (!gSelectedPieceHasMoved) {
 	        gMoveCount += 1;
@@ -102,95 +101,89 @@ function clickOnPiece(pieceIndex) {
 }
 
 function isThereAPieceBetween(cell1, cell2) {
-    /* note: assumes cell1 and cell2 are 2 squares away
-       either vertically, horizontally, or diagonally */
-    var rowBetween = (cell1.row + cell2.row) / 2;
-    var columnBetween = (cell1.column + cell2.column) / 2;
-    for (var i = 0; i < gNumPieces; i++) {
-	    if ((gPieces[i].row == rowBetween) &&
-	        (gPieces[i].column == columnBetween)) {
+    let rowBetween = (cell1.row + cell2.row) / 2;
+    let columnBetween = (cell1.column + cell2.column) / 2;
+
+    for (let i = 0; i < gNumPieces; i++) {
+	    if ((gPieces[i].row == rowBetween) && (gPieces[i].column == columnBetween)) {
 	        return true;
 	    }
     }
+
     return false;
 }
 
 function isTheGameOver() {
-    for (var i = 0; i < gNumPieces; i++) {
+    for (let i = 0; i < gNumPieces; i++) {
 	    if (gPieces[i].row > 2) {
 	        return false;
         }
 
-	    if (gPieces[i].column < (kBoardWidth - 3)) {
+        if (gPieces[i].column < (kBoardWidth - 3)) {
 	        return false;
 	    }
     }
+
     return true;
 }
 
 function drawBoard() {
-    if (gGameInProgress && isTheGameOver()) {
-	    endGame();
-    }
-
     gDrawingContext.clearRect(0, 0, kPixelWidth, kPixelHeight);
     gDrawingContext.beginPath();
     
-    /* vertical lines */
-    for (var x = 0; x <= kPixelWidth; x += kPieceWidth) {
+    for (let x = 0; x <= kPixelWidth; x += kPieceWidth) {
 	    gDrawingContext.moveTo(0.5 + x, 0);
 	    gDrawingContext.lineTo(0.5 + x, kPixelHeight);
     }
     
-    /* horizontal lines */
-    for (var y = 0; y <= kPixelHeight; y += kPieceHeight) {
+    for (let y = 0; y <= kPixelHeight; y += kPieceHeight) {
 	    gDrawingContext.moveTo(0, 0.5 + y);
 	    gDrawingContext.lineTo(kPixelWidth, 0.5 +  y);
     }
     
-    /* draw it! */
     gDrawingContext.strokeStyle = "#ccc";
     gDrawingContext.stroke();
-    
-    for (var i = 0; i < 9; i++) {
+
+    gDrawingContext.drawImage(farmImg, 300, 0, 150, 150);
+
+    for (let i = 0; i < 9; i++) {
 	    drawPiece(gPieces[i], i == gSelectedPieceIndex);
     }
 
     gMoveCountElem.innerHTML = gMoveCount;
-    saveGameState();
+
+    if (gGameInProgress && isTheGameOver()) {
+        endGame();
+        finishPopup();
+    }
+}
+
+function finishPopup() {
+    popupElement = document.getElementById('finish-overlay').style.display = 'block';
+    let speech = document.getElementById("final-speech");
+
+    if (gMoveCount > 28) {
+        speech.textContent = 'Р’С‹ РІС‹РёРіСЂР°Р»Рё! РќРѕ...Сѓ РІР°СЃ Р±РѕР»СЊС€Рµ С…РѕРґРѕРІ, С‡РµРј Сѓ РёРіСЂРѕРєР° СѓСЂРѕРІРЅСЏ Р±РѕРі (СЌС‚Рѕ РјРѕР№ РїР°РїР° = 28 С…РѕРґРѕРІ, С‡С‚Рѕ-С‚Рѕ РїРѕС€Р»Рѕ РЅРµ РїРѕ РїР»Р°РЅСѓ :))';
+    }
 }
 
 function drawPiece(p, selected) {
-    var column = p.column;
-    var row = p.row;
-    var x = (column * kPieceWidth) + (kPieceWidth/2);
-    var y = (row * kPieceHeight) + (kPieceHeight/2);
-    var radius = (kPieceWidth/2) - (kPieceWidth/10);
+    let x = p.column * kPieceWidth;
+    let y = p.row * kPieceHeight;
+
     gDrawingContext.beginPath();
-    gDrawingContext.arc(x, y, radius, 0, Math.PI*2, false);
+    gDrawingContext.rect(x, y, kPieceWidth, kPieceHeight);
     gDrawingContext.closePath();
-    gDrawingContext.strokeStyle = "#fff";
 
-    //alert(cow.width);
-    //gDrawingContext.fillStyle = cowPattern;
-
-    gDrawingContext.stroke();
-   
-    //alert(cow.width);
-    gDrawingContext.drawImage(cow, column * kPieceWidth, row * kPieceHeight, 50, 50);
-    //gDrawingContext.fillRect(0, 400, );
-    //gDrawingContext.fill();
     if (selected) {
         gDrawingContext.fillStyle = "#fff";
-        //gDrawingContext.fillStyle = "cowImage.jpg";
 	    gDrawingContext.fill();
     }
+
+    gDrawingContext.drawImage(cowImg, x, y, 50, 50);    
 }
 
 if (typeof resumeGame != "function") {
-    saveGameState = function() {
-	    return false;
-    }
     resumeGame = function() {
 	    return false;
     }
@@ -236,11 +229,11 @@ function initGame(canvasElement, moveCountElement) {
     gCanvasElement.height = kPixelHeight;   
     gCanvasElement.addEventListener("click", halmaOnClick, false);
     gMoveCountElem = moveCountElement;
-    gDrawingContext = gCanvasElement.getContext("2d");
 
-    cow = document.getElementById("cowImage");
-    //cow = new Image();
-    //cow.src = "Cows_files/cowImage.png";
+    gDrawingContext = gCanvasElement.getContext("2d");
+    farmImg = document.getElementById("farm-image");
+    cowImg = document.getElementById("cow-image");
+
 
     if (!resumeGame()) {
 	    newGame();
